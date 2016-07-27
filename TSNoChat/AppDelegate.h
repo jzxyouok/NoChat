130bbd78320bde790b7c3815afbd4c5433d60d69
WangShengquan <tanxiaosheng824@163.com>
2016-07-27 14:51:30 +0800

    Initial Commit

---
+++ TSNoChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TSNoChat
+//
+//  Created by WangShengquan on 16/7/27.
+//  Copyright © 2016年 WangShengquan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TSNoChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TSNoChat
+//
+//  Created by WangShengquan on 16/7/27.
+//  Copyright © 2016年 WangShengquan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
